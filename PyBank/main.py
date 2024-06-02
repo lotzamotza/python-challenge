1e70ad46773e047a81a47922d614b5f317a66dfa
lotzamotza <jramsey97@outlook.com>
2024-06-02 18:32:22 -0500

    second push

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,43 @@
- 
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+total_months = 0
+net_total = 0
+profit_loss_list = []
+month_list = []
+
+with open(csvpath, 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+        month = row[0]
+        try:
+           profit_loss = int(row[1].strip())
+        except ValueError:
+           continue
+
+        total_months +=1
+        net_total +=profit_loss
+        month_list.append(month)
+        profit_loss_list.append(profit_loss)
+    changes = {month_list[i]: profit_loss_list[i] - profit_loss_list[i-1] for i in range(1, len(profit_loss_list))}
+    average_change = sum(changes.values())/len(changes)
+
+with open("financial_data.txt", "w") as file:
+    print("Financial Analysis")
+    file.write("Financial Analysis\n")
+    print("--------------------------------------------------------")
+    file.write("--------------------------------------------------------\n")
+    print(f"Total Months: {total_months}")
+    file.write(f"Total Months: {total_months}\n")
+    print(f"Total: ${net_total}")
+    file.write(f"Total: ${net_total}\n")
+    print(f"Average Change: ${average_change}")
+    file.write(f"Average Change: ${average_change}\n")
+    print(f"Greatest Increase in Profits: {max(changes, key=changes.get)} (${max(changes.values())})")
+    file.write(f"Greatest Increase in Profits: {max(changes, key=changes.get)} (${max(changes.values())})\n")
+    print(f"Greatest Decrease in Profits: {min(changes, key=changes.get)} (${min(changes.values())})")
+    file.write(f"Greatest Decrease in Profits: {min(changes, key=changes.get)} (${min(changes.values())})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,42 @@
- 
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+total_votes = 0
+candidate_set = set()
+candidate_list = list()
+candidate_dict = dict()
+
+with open(csvpath, 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = (next(csvreader))
+
+    for row in csvreader:
+        voter_id = row[0]
+        county = row[1]
+        candidate = row[2]
+        total_votes += 1
+        candidate_set.add(candidate)
+        candidate_list.append(candidate)
+    
+with open("election_data.txt", "w") as file:
+    print("Election Results")
+    file.write("Election Results \n")
+    print("------------------------------------")
+    file.write("------------------------------------\n")
+    print("Total Votes:", total_votes)
+    file.write(f"Total Votes: , {total_votes} \n")
+    print("------------------------------------")
+    file.write("------------------------------------ \n")
+    
+    for i in candidate_set:
+        print(f"{i}: {round((candidate_list.count(i) / len(candidate_list))*100,3)}%, ({candidate_list.count(i)})")
+        file.write(f"{i}: {round((candidate_list.count(i) / len(candidate_list))*100,3)}%, ({candidate_list.count(i)}) \n")
+        candidate_dict[i] = candidate_list.count(i)
+    print("------------------------------------")
+    file.write("------------------------------------ \n")
+    print(f"Winner: {max(candidate_dict, key=candidate_dict.get)}")
+    file.write(f"Winner: {max(candidate_dict, key=candidate_dict.get)} \n")
+    print("------------------------------------")
+    file.write("------------------------------------\n")
